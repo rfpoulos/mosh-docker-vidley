cec7b514b918d000d335ac3f4adcb7bd305288bd
rfpoulos <rfpoulos@outlook.com>
2021-05-10 00:11:55 -0400

    coflicts

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,11 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-<<<<<<< HEAD
-  res.send("<h1>Api is running!!!</h1>");
-=======
   res.send("<h1>Api is running!</h1>");
->>>>>>> 0622d9999b8dfd68651eb5fb955857ed9658b3d9
 });
 
 module.exports = router;
